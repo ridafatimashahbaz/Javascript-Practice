0969455a28affd0067ca1550b18f46abbb6ff8ec
Rida Fatima Shahbaz <rida43430@gmail.com>
2026-08-09 23:42:02 +0500

    Add files via upload

---
+++ Fundamentals_Part-1/Equality-operators.js
@@ -0,0 +1,14 @@
+const age=18;
+if(age===18){ //STRICT Equality operators
+console.log("You just became an adult:D (strict)");
+}
+if(age==18){ //loose Equality operators
+console.log("You just became an adult:D (loose)");
+}
+
+const favourite =prompt("What's your favourite number?");
+console.log(favourite);
+console.log(typeof favourite);
+if(favourite===23){
+    console.log("Cool!23 is an amazing number!");
+}
